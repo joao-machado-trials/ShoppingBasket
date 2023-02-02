190dd80d7b74460ad52ecb3d9030ef80d3b86620
dvt_jmachado <joao.machado_ext@a-to-be.com>
2023-02-02 10:40:17 +0000

    Consolidate problem
    Change-Id: I974c1c120ad82ac212530f62742762262661a75c

---
+++ src/main/java/com/interview/shoppingbasket/BasketConsolidationCheckoutStep.java
@@ -0,0 +1,10 @@
+package com.interview.shoppingbasket;
+
+public class BasketConsolidationCheckoutStep implements CheckoutStep {
+
+    @Override
+    public void execute(CheckoutContext checkoutContext) {
+        Basket basket = checkoutContext.getBasket();
+        basket.consolidateItems();
+    }
+}
---
+++ src/main/java/com/interview/shoppingbasket/BasketItem.java
@@ -0,0 +1,36 @@
+package com.interview.shoppingbasket;
+
+import lombok.Data;
+
+@Data
+public class BasketItem {
+    private String productCode;
+    private String productName;
+    private int quantity;
+    private double productRetailPrice;
+
+    public String getProductCode() {
+		return productCode;
+	}
+	public void setProductCode(String productCode) {
+		this.productCode = productCode;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public double getProductRetailPrice() {
+		return productRetailPrice;
+	}
+	public void setProductRetailPrice(double productRetailPrice) {
+		this.productRetailPrice = productRetailPrice;
+	}
+}
